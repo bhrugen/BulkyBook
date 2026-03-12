9f420d91a9403b0e3b39414cbf4751e02e6ca3e1
Bhrugen Patel <bhrugen.patel@informa.com>
2026-03-12 12:25:55 -0500

    Section 7 - Home Page in Action

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BulkyBook.Business.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,9 +7,17 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync(includeCategory: true);
+            return View(products);
         }
 
         public IActionResult Privacy()
