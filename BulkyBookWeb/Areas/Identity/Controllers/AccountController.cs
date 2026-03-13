9fbc9923f2173c03d9a2f2196fd54a4408c6cb1e
Bhrugen Patel <bhrugen.patel@informa.com>
2026-03-12 20:50:27 -0500

    Section 8 - Register Action Method

--- BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
+++ BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Identity.Controllers
 {
     [Area("Identity")]
     public class AccountController : Controller
     {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager= signInManager;
+        }
+
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
             return View();
         }
@@ -13,6 +34,46 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = registerVM.Email,
+                    Email = registerVM.Email,
+                    Name = registerVM.Name,
+                    PhoneNumber = registerVM.PhoneNumber,
+                    StreetAddress = registerVM.StreetAddress,
+                    City = registerVM.City,
+                    State = registerVM.State,
+                    PostalCode = registerVM.PostalCode
+                };
+
+
+                var result = await _userManager.CreateAsync(user);
+
+
+                if (result.Succeeded)
+                {
+                    //user has been created
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("Index", "Home", new { area = "Customer" });
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+            }
+            return View(registerVM);
         }
 
         public IActionResult AccessDenied()
