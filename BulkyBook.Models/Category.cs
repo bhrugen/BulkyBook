86d030e81d9319198331f6f1bc24761aa1c3aaf9
bhrugen <hello@dotnetmastery.com>
2026-03-16 13:10:57 -0500

    trst

--- BulkyBook.Models/Category.cs
+++ BulkyBook.Models/Category.cs
@@ -6,7 +6,7 @@ namespace BulkyBook.Models
     public class Category
     {
         //[Key]
-        public int Id { get; set; }
+        public int Id { get; set; }ss
         [Required]
         [StringLength(100)]
         [Display(Name ="Category Name")]
