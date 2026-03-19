943b5b257f6f355bdc89d0bf919bc8cc23b21bbd
bhrugen <hello@dotnetmastery.com>
2026-03-19 15:10:15 -0500

    Section 10 - Add Items to Cart

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using BulkyBook.Business.Services.IServices;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
@@ -9,10 +12,11 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
-
-        public HomeController(IProductService productService)
+        private readonly IShoppingCartService _shoppingCartService;
+        public HomeController(IProductService productService, IShoppingCartService shoppingCartService)
         {
             _productService = productService;
+            _shoppingCartService = shoppingCartService;
         }
 
         public async Task<IActionResult> Index()
@@ -39,10 +43,24 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(cart);
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Details(ShoppingCart shoppingCart)
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            shoppingCart.ApplicationUserId = userId;
+            await _shoppingCartService.AddToCartAsync(shoppingCart);
+            
+            return RedirectToAction("Details",new { productId = shoppingCart.ProductId });
         }
+
 
       
     }
