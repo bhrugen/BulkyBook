553647366e1e902f00abd9b36762e4c855bb69a0
bhrugen <hello@dotnetmastery.com>
2026-03-23 20:34:29 -0500

    Section 15 - Admin Dashboard View Model

--- BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
+++ BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
@@ -49,6 +49,10 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
                     {
                         return Redirect(returnUrl);
                     }
+                    if(User.IsInRole(SD.RoleAdmin) || User.IsInRole(SD.RoleEmployee))
+                    {
+                        return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+                    }
                         return RedirectToAction("Index", "Home", new { area = "Customer" });
                 }
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
